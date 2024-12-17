71f9d3a00585087c667d0934f63349f037b9b697
breynersmith <breynersmithusariz@gmail.com>
2024-12-17 18:05:18 -0500

    cors updated

--- tasktrack/tasktrack/settings.py
+++ tasktrack/tasktrack/settings.py
@@ -145,6 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    'https://tastrack-project-breyner.vercel.app/'
 ]
 
 DJOSER = {
